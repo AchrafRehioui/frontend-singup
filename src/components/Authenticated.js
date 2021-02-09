a27a98ba47704340e22a73d2e8b4c02e52e97fe8
Achraf <achraf.rehioui.1@gmail.com>
2021-02-09 18:35:52 +0100

    Authenticated component

--- src/components/Authenticated.js
+++ src/components/Authenticated.js
@@ -1,8 +1,31 @@
-import React from 'react'
+import React, { useState } from 'react';
+import { Card, Button, Alert } from 'react-bootstrap';
+import { useAuth } from '../contexts/AuthContext';
 
 export default function Authenticated() {
+
+    const [error, setError] = useState("");
+    const { currentUser } = useAuth();
+
+    function handleLogout() {
+
+    }
+
     return (
-        <div>Authenticated</div>
+        <>
+            <Card>
+                <Card.Body>
+                    <h2 className="text-center mb-4">Profile</h2>
+                    {error && <Alert variant="danger">{error} </Alert>}
+                    <strong>Email:</strong> {currentUser.email}
+                </Card.Body>
+            </Card>
+            <div className="w-100 text-center mt-2">
+                <Button variant="link" onClick={handleLogout}>
+                    Log Out
+                </Button>
+            </div>
+        </>
     )
 }
 
