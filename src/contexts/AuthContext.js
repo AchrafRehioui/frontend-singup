6b300bdeaec642084be53bbd16ccd646f788b424
Achraf <achraf.rehioui.1@gmail.com>
2021-02-09 11:22:37 +0100

    auth loading state

--- src/contexts/AuthContext.js
+++ src/contexts/AuthContext.js
@@ -9,14 +9,18 @@ export function useAuth() {
 
 export function AuthProvider({ children }) {
     const [currentUser, setCurrentUser] = useState();
+    const [loading, setLoading] = useState(true);
 
     function signup(email, password) {
         return auth.createUserWithEmailAndPassword(email, password)
+
     }
 
     useEffect(() => {
         const unsubscribe = auth.onAuthStateChanged(user => {
             setCurrentUser(user)
+            setLoading(false)
+
         })
         return unsubscribe
 
@@ -29,7 +33,7 @@ export function AuthProvider({ children }) {
 
     return (
         <AuthContext.Provider value={value}>
-            { children}
+            {!loading && children}
         </AuthContext.Provider>
     )
 }
